f051257f420d4ff6e718d708938461b86cc099a9
zebera99 <76399201+zebera99@users.noreply.github.com>
2021-09-25 22:10:23 +0900

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,13 @@
+def is_palindrome(word):
+    if word[::-1] == word:
+        return True
+    else:
+        return False
+
+
+# test
+print(is_palindrome("racecar"))
+print(is_palindrome("stars"))
+print(is_palindrome("토마토"))
+print(is_palindrome("kayak"))
+print(is_palindrome("hello"))
